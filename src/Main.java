ebeac0845f15e59e118400f75fa5d1e1328bb1e4
VanLien1503 <hoanglien89nd@gmail.com>
2020-05-17 13:44:55 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1. Stack of integer");
+        MyGenericStack.stackOfIntegers();
+        System.out.println("1. Stack of String");
+        MyGenericStack.stackOfStrings();
+    }
+}
